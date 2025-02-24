8c90573c6e66f796c50d80d67e70c8d2f48a2b17
vini230403 <vini2003.melo@gmail.com>
2025-02-24 15:42:55 -0300

    finalizado

---
+++ secretword/src/components/GameOver.jsx
@@ -0,0 +1,13 @@
+
+const GameOver = ({retryGame, score}) => {
+  return (
+    <div>
+      <h1>Fim de jogo!</h1>
+      <h2>A sua pontuação foi: <span>{score}</span>
+      </h2>
+      <button onClick={retryGame}>Resetar jogo</button>
+    </div>
+  )
+}
+
+export default GameOver
